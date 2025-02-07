ee234c9f2b111254d96359637e40f80a94422f1c
Jing Feng <jingfeng.cheah@schultegroup.sg>
2025-02-07 16:43:43 +0800

    feat: change header scope from block to inline

--- lib/src/document/attribute.dart
+++ lib/src/document/attribute.dart
@@ -354,7 +354,7 @@ class PlaceholderAttribute extends Attribute<bool> {
 
 class HeaderAttribute extends Attribute<int?> {
   const HeaderAttribute({int? level})
-      : super('header', AttributeScope.block, level);
+      : super('header', AttributeScope.inline, level);
 }
 
 class IndentAttribute extends Attribute<int?> {
